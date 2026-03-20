e9dd1f7174e1f63bae0b9d8b319137d561ac4d23
Francielly5 <franciellym.ferreira15@gmail.com>
2026-03-19 21:54:59 -0300

    ProjetoBiblioteca 19/03

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Biblioteca</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    include("menu.php");
+    ?>
+    <div class="content">
+        <h1>Biblioteca Digital Municipal</h1>
+        <p>A Biblioteca Digital Municipal tem como missão promover o acesso ao conhecimento, à cultura e à educação por meio de um acervo digital acessível a toda a comunidade.</p>
+        <br>
+        <p>Nosso sistema permite consultar obras literárias, acessar livros digitais e incentivar
+            a leitura através da tecnologia.
+            Este ambiente foi desenvolvido para facilitar o gerenciamento do acervo bibliográfico
+            e aproximar leitores do universo literário.
+        </p>
+
+    </div>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCdU_8dHOx6YXioT5OlcKUPGca7KMTej91BQ&s" 
+     alt="Uma linda biblioteca" style="width:100%;border-radius:12px">
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<div class="sidebar">
+
+    <h2>Biblioteca</h2>
+    <a href="index.php">Início</a>
+    <a href="livros.php">Acervo</a>
+    <a href="cadastrar.php">Cadastrar Livro</a>
+
+</div>
+
